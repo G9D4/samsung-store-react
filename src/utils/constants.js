0eff5b725318d13f4b1a9d8a636e1401924b0bea
Gabriela Díaz <gldt2494@gmail.com>
2024-12-20 18:09:40 -0500

    feat: agrega register y checkout

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,3 +12,11 @@ export const filters=[
     {id:3,value:types.at(2).value,label:'Electrodomesticos'},
     {id:4,value:types.at(3).value,label:'Tecnología AI'}
 ]
+
+export const products=[
+    { id:"1",type:"mobile", title:'Galaxy S24 E', price:100, src:"https://images.samsung.com/is/image/samsung/p6pim/pe/sm-s721blbqltp/gallery/pe-galaxy-s24-fe-s721-sm-s721blbqltp-thumb-543769618?$180_180_PNG$"},
+    { id:"2",type:"mobile",title:'Galaxy Z Fold6', price:130,src:"https://images.samsung.com/is/image/samsung/p6pim/pe/sm-f956bzsvpeo/gallery/pe-galaxy-z-fold6-f956-sm-f956bzsvpeo-thumb-543216135?$180_180_PNG$"},
+    { id:"3",type:"mobile",title:'producto 3', price:130,src:"https://images.samsung.com/is/image/samsung/p6pim/pe/sm-f741bzskpeo/gallery/pe-galaxy-zflip6-f741-sm-f741bzskpeo-thumb-543214352?$180_180_PNG$"},
+    { id:"4",type:"tv_audio",title:'TV Audio', price:130,src:"https://images.samsung.com/is/image/samsung/p6pim/pe/qn65q65dagxpe/gallery/pe-qled-q60d-qn65q65dagxpe-544280736?$650_519_PNG$"},
+    { id:"5",type:"electr",title:'producto 3', price:130,src:"https://images.samsung.com/is/image/samsung/p6pim/pe/rf32cg5910b1pe/gallery/pe-3door-french-door-large-capacity-with-spacemax-technology-rf32cg5910b1pe-538509120?$650_519_PNG$"},
+]
