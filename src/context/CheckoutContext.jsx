12bc6c01e0c84c5c10cb9a75576cb2f3d036e578
Gabriela Díaz <gldt2494@gmail.com>
2024-12-23 23:38:37 -0500

    feat: arregla errores en consola

--- src/context/CheckoutContext.jsx
+++ src/context/CheckoutContext.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { createContext, useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
