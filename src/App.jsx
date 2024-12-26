e0d07ce1115bb3a7c894dc94bf7634fa7e97090b
Gabriela Díaz <gldt2494@gmail.com>
2024-12-26 15:14:04 -0500

    feat: arregla rutas

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,24 @@ function App() {
           <CartProvider>
             <CheckoutProvider>
               <Routes>
-                  <Route path="/"  element={ <AppLayout /> } />
-                  <Route index  element={<Home />} />
-                  <Route path="login"  element={<Login />} />
-                  <Route path="register"  element={<Register />} />
-                  <Route path="shoppingCart"  element={<ShoppingCart />} />
-                  <Route path="checkout"  element={<Checkout />} />
+                <Route path="/" element={<AppLayout />}>
+                  <Route index element={<Home />} />
+                  <Route path="login" element={<Login />} />
+                  <Route path="register" element={<Register />} />
+                  <Route path="shoppingCart" element={<ShoppingCart />} />
+                  <Route path="checkout" element={<Checkout />} />
+                </Route>
               </Routes>
             </CheckoutProvider>
           </CartProvider>
         </AuthProvider>
       </BrowserRouter>
-      <Toaster position="top-center" gutter={12} containerStyle={{margin:"8px"}}
-          toastOptions={{success:{duration:3000,},error:{duration:5000,},
-          style:{fontSize: '16px', maxWidth:'500px', padding: '16px 24px',backgroundColor: "white",color: "gray",}}}
-        />
+      <Toaster position="top-center" gutter={12} containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 3000, }, error: { duration: 5000, },
+          style: { fontSize: '16px', maxWidth: '500px', padding: '16px 24px', backgroundColor: "white", color: "gray", }
+        }}
+      />
     </>
   )
 }
